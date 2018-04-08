1d64d5e1cec16f756501ec84d7a226ef28a609bc
roynirmal <nroy000@gmail.com>
2018-04-08 04:37:50 +0200

    slow

--- Network_metrics.R
+++ Network_metrics.R
@@ -22,6 +22,7 @@ metricClose <- data.frame(UserId = integer(), CreationDate = double(), Closeness
 metricEig <- data.frame(UserId = integer(), CreationDate = double(), EigenCen = integer(), stringsAsFactors = FALSE)
 metricClus <- data.frame(UserId = integer(), CreationDate = double(), Clustering = integer(), stringsAsFactors = FALSE)
 
+
 #Calculation of Network Metrics and how they changed over time
 count = 0
 for(theDate in unique(reduced$CreationDate)){
@@ -38,27 +39,35 @@ for(theDate in unique(reduced$CreationDate)){
   b = betweenness(tempg)
   #just storing the values of the nodes which gve an answer in the current timestamp of the for loop
   needb = b[paste(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x[1:length(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x)])]
-  metricBet = rbind(metricBet, data.frame(UserId = names(needb),CreationDate = rep(theDate, length(needb)), Betweenness = needb))
+  #metricBet = rbind(metricBet, data.frame(UserId = names(needb),CreationDate = rep(theDate, length(needb)), Betweenness = needb))
+  metricBet[nrow(metricBet)+1:length(needb),] = data.frame(UserId = names(needb),CreationDate = rep(theDate, length(needb)), Betweenness = needb)
   
   #Closeness
   c = closeness(tempg)
   #just storing the values of the nodes which gve an answer in the current timestamp of the for loop
   needc = c[paste(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x[1:length(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x)])]
-  metricClose = rbind(metricClose, data.frame(UserId = names(needc),CreationDate = rep(theDate, length(needc)), Closness = needc))
+  #metricClose = rbind(metricClose, data.frame(UserId = names(needc),CreationDate = rep(theDate, length(needc)), Closness = needc))
+  metricClose[nrow(metricClose)+1:length(needc),] = data.frame(UserId = names(needc),CreationDate = rep(theDate, length(needc)), Closeness = needc)
   
   #Eigen Centrality
-  e = eigen_centrality(tempg)$values
+  e = eigen_centrality(tempg)$vector
   #just storing the values of the nodes which gve an answer in the current timestamp of the for loop
   neede = e[paste(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x[1:length(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x)])]
-  metricEig = rbind(metricEig, data.frame(UserId = names(neede),CreationDate = rep(theDate, length(neede)), EigenCen = neede))
+  #metricEig = rbind(metricEig, data.frame(UserId = names(neede),CreationDate = rep(theDate, length(neede)), EigenCen = neede))
+  metricEig[nrow(metricEig)+1:length(neede),] = data.frame(UserId = names(neede),CreationDate = rep(theDate, length(neede)), EigenCen = neede)
   
   #Clustering Coefficient
-  t = transitivity(tempg)
+  t = transitivity(tempg, type = "local")
+  names(t) = names(b)
   #just storing the values of the nodes which gve an answer in the current timestamp of the for loop
   needt = t[paste(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x[1:length(temp[which(temp$CreationDate == theDate),]$OwnerUserId.x)])]
-  metricClus = rbind(metricClus, data.frame(UserId = names(needt),CreationDate = rep(theDate, length(needt)), Clustering = needt))
-  
+  #metricClus = rbind(metricClus, data.frame(UserId = names(needt),CreationDate = rep(theDate, length(needt)), Clustering = needt))
+  metricClus[nrow(metricClus)+1:length(needt),] = data.frame(UserId = names(needt),CreationDate = rep(theDate, length(needt)), Clustering = needt)
   count = count + 1
+  print(count)
+  # if(count>10){
+  #   break
+  # }
 }
 
 write.csv(metricBet, file = "Between.csv")
